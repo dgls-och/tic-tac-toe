eee84c6f61e89c914079e865099700a3c1f704b3
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-08 21:02:39 +0100

    Rewrite Gameboard module

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-const GameBoard = (function () {
+const Gameboard = (() => {
     const board = [
         ["", "", ""],
         ["", "", ""],
         ["", "", ""]
     ];
-    return board;
+
+    const getBoard = () => board;
+
+    const placeMarker = (row, col, marker) => {
+        if (board[row][col] !== "") return false;
+
+        board[row][col] = marker;
+        return true;
+    };
+
+    const resetBoard = () => {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                board[row][col] = "";
+            }
+        }
+    };
+
+    const printBoard = () => {
+        console.log(board);
+    };
+
+    return {
+        getBoard,
+        placeMarker,
+        resetBoard,
+        printBoard
+    };
 })();
+
+function Player(name, marker) {
+    return {
+        name,
+        marker
+    };
+}
+
